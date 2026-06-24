2ea207c4d2a1be05a227728972f13a79a2d73b16
Afwanizzulfadhoil <afwan2031@gmail.com>
2026-06-24 14:29:14 +0700

    feat: new

--- src/main/java/rumputhijau/pbokmnarkotikarumputhijau/controller/KnowledgeController.java
+++ src/main/java/rumputhijau/pbokmnarkotikarumputhijau/controller/KnowledgeController.java
@@ -1,42 +1,5 @@
 package rumputhijau.pbokmnarkotikarumputhijau.controller;
 
-import rumputhijau.pbokmnarkotikarumputhijau.model.KnowledgeRepository;
-import rumputhijau.pbokmnarkotikarumputhijau.model.Putusan;
-import rumputhijau.pbokmnarkotikarumputhijau.model.StatistikPutusan;
-
-import java.util.ArrayList;
-
 public class KnowledgeController {
-    public final KnowledgeRepository repository;
 
-    public KnowledgeController(KnowledgeRepository repository) {
-        this.repository = repository;
-    }
-
-    //Menambahkan putusan baru dari data form
-    public boolean tambahPutusan(String[] data) {
-        try {
-
-            Putusan putusan = new Putusan(
-                    data[0],                        // nomorPerkara
-                    data[1],                        // pengadilan
-                    data[2],                        // tanggalPutusan
-                    data[3],                        // namaTerdakwa
-                    Integer.parseInt(data[4]),      // umurTerdakwa
-                    data[5],                        // jenisNarkotika
-                    Double.parseDouble(data[6]),    // beratBarangBukti
-                    data[7],                        // pasalDilanggar
-                    data[8],                        // peranTerdakwa
-                    Integer.parseInt(data[9]),      // vonisHukuman
-                    Double.parseDouble(data[10]),   // vonisDenda
-                    data[11]                        // namaHakim
-            );
-
-            repository.simpan(putusan);
-            return true;
-        } catch (Exception e) {
-            System.out.println("Gagal menambahkan data: " + e.getMessage());
-            return false;
-        }
-    }
 }
