7894e7ec25f6819eaba48e0bb76c4d22ccc7c327
nizhamwananta <nizhamwananta@gmail.com>
2026-06-24 13:57:48 +0700

    feat: menambahkan method dan validasi di StatistikPutusan

--- src/main/java/rumputhijau/pbokmnarkotikarumputhijau/model/Putusan.java
+++ src/main/java/rumputhijau/pbokmnarkotikarumputhijau/model/Putusan.java
@@ -48,8 +48,6 @@ public class Putusan {
 
     @Override
     public  String toString() {
-        return nomorPerkara + " | " + namaTerdakwa " | " + jenisNarkotika + " | " + vonisHukuman + " tahun ";
+        return nomorPerkara + " | " + namaTerdakwa + " | " + jenisNarkotika + " | " + vonisHukuman + " tahun ";
     }
-
-//    testing 1 2 3
 }
--- src/main/java/rumputhijau/pbokmnarkotikarumputhijau/model/StatistikPutusan.java
+++ src/main/java/rumputhijau/pbokmnarkotikarumputhijau/model/StatistikPutusan.java
@@ -1,4 +1,27 @@
 package rumputhijau.pbokmnarkotikarumputhijau.model;
+import java.util.ArrayList;
 
 public class StatistikPutusan {
+    public static double rataRataVonis(ArrayList<Putusan> data) {
+        if (data.isEmpty())
+            return 0;
+        int total = 0;
+
+        for (Putusan p : data) {
+            total += p.getVonisHukuman();
+        }
+        return (double) total / data.size();
+    }
+    public static Putusan vonisTerberat (ArrayList<Putusan> data) {
+        if (data.isEmpty())
+            return null;
+        Putusan terbesar = data.get(0);
+
+        for (Putusan p : data) {
+            for (p.getVonisHukuman() > terbesar.getVonisHukuman()) {
+                terbesar = p;
+            }
+        }
+        return terbesar;
+    }
 }
