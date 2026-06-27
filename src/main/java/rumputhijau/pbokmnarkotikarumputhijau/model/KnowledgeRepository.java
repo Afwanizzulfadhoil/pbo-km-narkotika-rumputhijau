9b4c5ac0f2598ea721f3434fb2e08780839a375d
nizhamwananta <nizhamwananta@gmail.com>
2026-06-27 16:10:19 +0700

    feat: menambahkan create,search,update dan delete

--- src/main/java/rumputhijau/pbokmnarkotikarumputhijau/model/KnowledgeRepository.java
+++ src/main/java/rumputhijau/pbokmnarkotikarumputhijau/model/KnowledgeRepository.java
@@ -41,7 +41,7 @@ public class KnowledgeRepository {
                 daftarPutusan.add(putusan);
             }
         } catch (IOException e) {
-            System.out.println("Gagal membaca file CSV" + e.getMessage());
+            System.out.println("Gagal Membaca File CSV" + e.getMessage());
         }
     }
 
