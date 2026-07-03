c6b0168c2b032a5372642a2e1685fac834cf1b14
Firjatullah <firjatullahxy@gmail.com>
2026-07-03 09:14:38 +0700

    fix: memperbaiki kesalahan penggunaan braces

--- src/main/java/rumputhijau/pbokmnarkotikarumputhijau/app/Main.java
+++ src/main/java/rumputhijau/pbokmnarkotikarumputhijau/app/Main.java
@@ -14,89 +14,59 @@ public class Main {
 
         KnowledgeRepository repository =
                 new KnowledgeRepository();
-
         KnowledgeController controller =
                 new KnowledgeController(repository);
-
         ConsoleView view =
                 new ConsoleView();
-
         Scanner sc = new Scanner(System.in);
-
         controller.loadData("resources/Putusan.csv");
 
         int pilihan;
 
         do {
-
             pilihan = view.tampilkanMenu();
-
             switch (pilihan) {
 
                 case 1:
-
                     view.tampilkanDaftarPutusan(controller.getSemuaPutusan());
-
                     break;
 
                 case 2:
-
                     System.out.print("Masukkan Nomor Perkara : ");
-
                     String nomor = sc.nextLine();
 
                     Putusan p = controller.cariNomorPerkara(nomor);
-
                     view.tampilkanDetail(p);
-
                     break;
 
                 case 3:
 
-                    System.out.println("Jumlah Data : "
-                            + controller.getJumlahData());
-
-                    System.out.println("Rata-rata Vonis : "
-                            + controller.getRataRataVonis());
-
+                    System.out.println("Jumlah Data : " + controller.getJumlahData());
+                    System.out.println("Rata-rata Vonis : " + controller.getRataRataVonis());
                     System.out.println("Vonis Terberat :");
-
                     System.out.println(controller.getVonisTerberat());
-
                     break;
 
                 case 4:
-
                     String[] data = view.inputFormPutusan(sc);
 
                     if(controller.tambahPutusan(data)){
-
                         view.tampilkanPesan("Data berhasil ditambahkan.");
-
                     }
 
                     else{
-
                         view.tampilkanPesan("Data gagal ditambahkan.");
-
                     }
 
                     break;
 
                 case 5:
-
                     System.out.println("Terima kasih.");
-
                     break;
 
                 default:
-
                     System.out.println("Menu tidak tersedia.");
-
             }
-
         } while (pilihan != 5);
-
-    }
     }
 }
