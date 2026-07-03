4a828f83e69fedae9d8b3d401b8f3daf66951331
Firjatullah <firjatullahxy@gmail.com>
2026-07-03 13:21:11 +0700

    "Fix: memperbaiki Main dan Launcher"

--- src/main/java/rumputhijau/pbokmnarkotikarumputhijau/app/Main.java
+++ src/main/java/rumputhijau/pbokmnarkotikarumputhijau/app/Main.java
@@ -1,72 +1,7 @@
 package rumputhijau.pbokmnarkotikarumputhijau.app;
 
-import rumputhijau.pbokmnarkotikarumputhijau.controller.KnowledgeController;
-import rumputhijau.pbokmnarkotikarumputhijau.model.KnowledgeRepository;
-import rumputhijau.pbokmnarkotikarumputhijau.model.Putusan;
-import rumputhijau.pbokmnarkotikarumputhijau.view.ConsoleView;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-
-        KnowledgeRepository repository =
-                new KnowledgeRepository();
-        KnowledgeController controller =
-                new KnowledgeController(repository);
-        ConsoleView view =
-                new ConsoleView();
-        Scanner sc = new Scanner(System.in);
-        controller.loadData("resources/Putusan.csv");
-
-        int pilihan;
-
-        do {
-            pilihan = view.tampilkanMenu();
-            switch (pilihan) {
-
-                case 1:
-                    view.tampilkanDaftarPutusan(controller.getSemuaPutusan());
-                    break;
-
-                case 2:
-                    System.out.print("Masukkan Nomor Perkara : ");
-                    String nomor = sc.nextLine();
-
-                    Putusan p = controller.cariNomorPerkara(nomor);
-                    view.tampilkanDetail(p);
-                    break;
-
-                case 3:
-
-                    System.out.println("Jumlah Data : " + controller.getJumlahData());
-                    System.out.println("Rata-rata Vonis : " + controller.getRataRataVonis());
-                    System.out.println("Vonis Terberat :");
-                    System.out.println(controller.getVonisTerberat());
-                    break;
-
-                case 4:
-                    String[] data = view.inputFormPutusan(sc);
-
-                    if(controller.tambahPutusan(data)){
-                        view.tampilkanPesan("Data berhasil ditambahkan.");
-                    }
-
-                    else{
-                        view.tampilkanPesan("Data gagal ditambahkan.");
-                    }
-
-                    break;
-
-                case 5:
-                    System.out.println("Terima kasih.");
-                    break;
-
-                default:
-                    System.out.println("Menu tidak tersedia.");
-            }
-        } while (pilihan != 5);
+        Launcher.main(args);
     }
 }
